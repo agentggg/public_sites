d7750bb2b381bdef2b65403accc87f2ea5493ec3
Tech & Faith <47467793+agentggg@users.noreply.github.com>
2026-03-25 14:46:49 -0400

    Update Tech & Faith logo asset reference

--- app_support/redirects.js
+++ app_support/redirects.js
@@ -9,7 +9,7 @@ const TECH_AND_FAITH = {
   legalName: "Tech & Faith LLC",
   displayName: "Tech & Faith",
   supportEmail: "tech.and.faith.contact@gmail.com",
-  logoSrc: "assets/images/white background.png",
+  logoSrc: "assets/images/techandfaithlogo.png",
   logoAlt: "Tech & Faith logo",
   copyright: "Copyright 2026 Tech & Faith LLC. All rights reserved."
 };
